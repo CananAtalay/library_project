5afbc896141c2f51338a06f083dc28f1e3e1df1a
CananAtalay <Canan Atalay@192.168.48.162>
2020-01-27 18:19:30 +0100

    Erster Entwurf/Aufbau

---
+++ LibraryProject/src/Book_db.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+public interface Book_db {
+	
+	
+	
+	
+public static void dasIstEinBuch (String book) {
+	
+
+}
+	
+	
+}
+
---
+++ LibraryProject/src/Reader.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public interface Reader {
+
+	public static void readerApplikation () {
+	
+	File file = new File("C:\\Users\\Canan Atalay\\Desktop\\Library_Projekt\\library_project\\library_db.txt"); 
+
+
+	try {
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+		String line;
+		while ((line = bufferedReader.readLine()) != null) {
+			System.out.println(line);
+		}
+
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+}
+
+}
