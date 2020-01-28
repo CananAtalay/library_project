407164313bbaeb73a4b53d103bc9e3f9a8f10b31
CananAtalay <Canan Atalay@NB-Canan01.fritz.box>
2020-01-28 19:59:57 +0100

    28.01.20 Version 3

--- LibraryProject/src/Book.java
+++ LibraryProject/src/Book.java
@@ -16,38 +16,35 @@ public class Book {
 
 	}
 
-	//Methode 
+	// Methode
 	public void getIsbn() {
 
 		System.out.println("Das ist die ISBN vom Buch: " + this.isbn);
 
 	}
-	
+
 	public void getAuthor() {
 
 		System.out.println("Das ist der Author vom Buch: " + this.author);
 
 	}
-	
+
 	public void getPublicationYear() {
 
 		System.out.println("Das ist das Erscheinungsdatum vom Buch: " + this.publicationYear);
 
 	}
-	
-	public void setIsbn (String isbn) {
-		this.isbn = isbn; 
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
 	}
-	
-	public void setAuthor (String author) {
+
+	public void setAuthor(String author) {
 		this.author = author;
 	}
-	
-	public void setPublicationYear (int publicationYear) {
+
+	public void setPublicationYear(int publicationYear) {
 		this.publicationYear = publicationYear;
 	}
-	
-	
-	
 
 }
