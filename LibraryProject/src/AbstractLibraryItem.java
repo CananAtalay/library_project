b4bffad10bcf286101a2abdb92d45b5618338d12
CananAtalay <Canan Atalay@192.168.48.162>
2020-01-28 16:38:13 +0100

    Library Eigenschaften speichern

--- LibraryProject/src/AbstractLibraryItem.java
+++ LibraryProject/src/AbstractLibraryItem.java
@@ -2,11 +2,28 @@ import java.util.Date;
 
 public abstract class AbstractLibraryItem {
 
-	String rentedBy; 
-	String itemType; 
-	String name; 
-	Date returnDate; 
+	// Eiegnschaften/Atribute
+
+	String rentedBy;
+	String itemType;
+	String name;
+	Date returnDate;
 	boolean isLoaned;
-	
-	
+
+	// Konstruktoren
+
+	public AbstractLibraryItem(String rentedBy, String itemType, String name, Date returnDate, boolean isLoanded) {
+		this.rentedBy = rentedBy;
+		this.itemType = itemType;
+		this.name = name;
+		this.returnDate = returnDate;
+		this.isLoaned = isLoanded;
+	}
+
+	// Methode
+
+	public static void getIsbn () {
+		
+	}
+
 }
